035f0abc0cc6fbccdea8daaebfee1121c6bed429
Cleveland <cameron3579@hotmail.com>
2016-08-04 17:45:51 -0500

    Added files to repo

---
+++ WilliamsCleveland/Assignments/Python/OddEven/odd_even.py
@@ -0,0 +1,8 @@
+number = 2000
+for i in range(0, 2000):
+	if i % 2 == 0:
+		print('This is an even ' + str(i))
+	else:
+		print('This is an odd ' + str(i))
+
+
---
+++ WilliamsCleveland/Assignments/jQuery/Sortable_Ninja (2)/Sortable_Ninja.js
@@ -0,0 +1,7 @@
+  $(document).ready(function() {
+    $( "#sort" ).sortable()
+   		 $( "#sort" ).disableSelection()
+  	
+  
+ });
+  
