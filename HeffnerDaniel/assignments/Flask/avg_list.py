d240bb6b3b5fcc3ad74992e4d34846500b26b578
Daniel Heffner <cdanielheffner@gmail.com>
2016-08-04 13:02:37 -0500

    the rest of the python assignments so far

---
+++ HeffnerDaniel/assignments/Flask/avg_list.py
@@ -0,0 +1,5 @@
+a = [1, 2, 5, 10, 255, 3]
+total = 0
+for count in range(0,len(a)):
+	total = total + a[count]
+print total/len(a)
---
+++ HeffnerDaniel/assignments/Flask/multiples_pt_2.py
@@ -0,0 +1,5 @@
+for count in range(5,1000001):
+	if count%5 != 0:
+		continue
+	else:
+		print count
